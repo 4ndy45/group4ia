42ba3b465870f66ae45b01047d747bb492fc5401
4ndy45 <34747520+4ndy45@users.noreply.github.com>
2017-12-22 21:32:41 -0500

    Add files via upload

---
+++ group4ib.py
@@ -0,0 +1,13 @@
+from processing import *
+
+width = 500
+height = 400
+
+def setup():
+    size(width,height)
+    background(255,255,255)
+    
+def draw():
+    background(255,255,255)
+    
+    
